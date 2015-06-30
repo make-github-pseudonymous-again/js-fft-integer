41c461b524cf4c713315ea649f5d6a0eb2c171b5
aureooms <aurelien.ooms@gmail.com>
2015-06-30 22:33:55 +0000

    drafts

---
+++ js/src/fft.js
@@ -0,0 +1,45 @@
+
+const _fft = function ( p , root , unzip ) {
+
+	const fft = function ( l , m , u , ui , uj , v , vi , vj ) {
+
+		if ( l === 0 ) {
+
+			v[vi] = u[ui] ;
+			return ;
+
+		}
+
+		let um = ui + m ;
+		let vm = vi + m ;
+
+		unzip( u , ui , uj , v , vi , vm ) ;
+
+		fft( l - 1 , m >>> 1 , v , vi , vm , u , ui , um ) ;
+		fft( l - 1 , m >>> 1 , v , vm , vj , u , um , uj ) ;
+
+		const z = root[l] ;
+
+		let w = 1 ;
+
+		const _vm = vm ;
+
+		while ( vi < _vm ) {
+
+			v[vi] = ( u[ui] + w * u[um] ) % p ;
+			v[vm] = ( u[ui] - w * u[um] ) % p ;
+
+			w *= z ;
+			w %= p ;
+
+			++um ; ++vm ; ++ui ; ++vi ;
+
+		}
+
+	} ;
+
+	return fft ;
+
+} ;
+
+exports.fft = fft ;
---
+++ js/src/ifft.js
@@ -0,0 +1,45 @@
+
+const _ifft = function ( p , inv2 , root , unzip ) {
+
+	const ifft = function ( l , m , u , ui , uj , v , vi , vj ) {
+
+		if ( l === 0 ) {
+
+			v[vi] = u[ui] ;
+			return ;
+
+		}
+
+		let um = ui + m ;
+		let vm = vi + m ;
+
+		unzip( u , ui , uj , v , vi , vm ) ;
+
+		ifft( l - 1 , m >>> 1 , v , vi , vm , u , ui , um ) ;
+		ifft( l - 1 , m >>> 1 , v , vm , vj , u , um , uj ) ;
+
+		const z = root[l] ;
+
+		let w = 1 ;
+
+		const _vm = vm ;
+
+		while ( vi < _vm ) {
+
+			v[vi] = ( inv2 * ( u[ui] + w * u[um] ) ) % p ;
+			v[vm] = ( inv2 * ( u[ui] - w * u[um] ) ) % p  ;
+
+			w *= z ;
+			w %= p ;
+
+			++um ; ++vm ; ++ui ; ++vi ;
+
+		}
+
+	} ;
+
+	return ifft ;
+
+} ;
+
+exports.ifft = ifft ;
