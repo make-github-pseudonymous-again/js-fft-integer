288f4db52024a0b742621245a0cd0e77399c5faf
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-02-16 11:35:39 +0100

    aureooms- > @aureooms/ : update js code

--- js/dist/fft-integer.js
+++ js/dist/fft-integer.js
@@ -13,10 +13,10 @@ if ( typeof exports === "object" ) {
 	definition( exports ) ;
 }
 else if ( typeof define === "function" && define.amd ) {
-	define( "aureooms-js-fft-integer" , [ ] , function ( ) { return definition( { } ) ; } ) ;
+	define( "@aureooms/js-fft-integer" , [ ] , function ( ) { return definition( { } ) ; } ) ;
 }
 else if ( typeof window === "object" && typeof window.document === "object" ) {
 	definition( window["fftinteger"] = { } ) ;
 }
-else console.error( "unable to detect type of module to define for aureooms-js-fft-integer") ;
+else console.error( "unable to detect type of module to define for @aureooms/js-fft-integer") ;
 } )( ) ;
